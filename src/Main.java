b076bd8adf4e49e55c61f0023be909a902d4c990
MiloslavStekrt <stekrt18@gmail.com>
2020-07-05 20:41:20 +0200

    Add Two button del and undo. You can download and run project or just download jar file.

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,16 @@ public class Main extends JFrame{
     private JPanel HlavniPanel;
     private JLabel hoverLabel;
     private JList list2;
+    private JButton delItem;
+    private JButton undoButton;
+    private String last_list1, last_list2;
 
     public Main(){
         this.setContentPane(HlavniPanel);
         this.pack();
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
@@ -26,9 +30,46 @@ public class Main extends JFrame{
         });
         button1.addMouseListener(new MouseAdapter() {
         });
+        delItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DeleteItem();
+            }
+        });
+        undoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getBackItem();
+            }
+        });
     }
 
+    public void getBackItem() {
+        if (last_list1 != null)
+            ((DefaultListModel)list1.getModel()).addElement(last_list1);
+        if (last_list2 != null)
+            ((DefaultListModel)list2.getModel()).addElement(last_list2);
+        last_list1 = null;
+        last_list2 = null;
+
+    }
+
+    private void DeleteItem() {
+        //save item value to string and then remove item from list1 or list2
+        if (list1.getSelectedIndex() > -1){
+            last_list1 = (String) list1.getSelectedValue();
+            ((DefaultListModel) list1.getModel()).remove(list1.getSelectedIndex());
+        }
+        if (list2.getSelectedIndex() > -1) {
+            last_list2 = (String) list2.getSelectedValue();
+            ((DefaultListModel) list2.getModel()).remove(list2.getSelectedIndex());
+        }
+    }
+
+
+
     public void convert(){
+        //this convert items random to 2 list
         List<String> itemsR= new ArrayList(Arrays.asList(textArea1.getText().split(",")));
         Collections.shuffle(itemsR);
         items = new String[itemsR.size()];
@@ -43,6 +84,7 @@ public class Main extends JFrame{
             else
                 m1.addElement(items[i]);
         }
+        //set list to view list
         list1.setModel(m);
         list2.setModel(m1);
     }
